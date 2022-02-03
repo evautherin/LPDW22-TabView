caf15a015c6325ee88edd39cdea9edf006e4960a
Etienne Vautherin <etienne@vautherin.com>
2022-02-03 10:05:04 +0100

    TabView

--- TabView/ContentView.swift
+++ TabView/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "person")
+                }
+
+            StoryView()
+                .tabItem {
+                    Label("Story", systemImage: "book")
+                }
+            
+            FavoritesView()
+                .tabItem {
+                    Label("Favorites", systemImage: "star")
+                }
+            
+            FunFactsView()
+                .tabItem {
+                    Label("Fun Facts", systemImage: "hand.thumbsup")
+                }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct HomeView: View {
+    var body: some View {
+        Text("HomeView")
+    }
+}
+
+struct StoryView: View {
+    var body: some View {
+        Text("StoryView")
+    }
+}
+
+struct FavoritesView: View {
+    var body: some View {
+        Text("FavoritesView")
+    }
+}
+
+struct FunFactsView: View {
+    var body: some View {
+        Text("FunFactsView")
     }
 }
