1804d4d972fc78b61b2e6871a3247e9d50c8c9ee
Etienne Vautherin <etienne@vautherin.com>
2022-02-03 09:55:11 +0100

    Initial Commit

---
+++ TabView/TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewApp.swift
+//  TabView
+//
+//  Created by Etienne Vautherin on 03/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
